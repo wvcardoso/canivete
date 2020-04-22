b5326a98fb5e609b26c096b8e057ae7937432541
Willian Vieira Cardoso <willian.cardoso@serpro.gov.br>
2020-04-21 21:39:34 -0300

    .

--- shell_scripts/docker-emby.sh
+++ shell_scripts/docker-emby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOVIES=/home/willian/Movies
+MOVIES=/home/willian/Videos/
 
-docker run -d --volume $MOVIES:/mnt/share2 --device /dev/dri:/dev/dri --publish 8096:8096 --publish 8920:8920 --env UID=1000 --env GID=100 --env GIDLIST=100 emby/embyserver:latest
+docker run -d --volume ./data:/config/data --volume $MOVIES:/mnt/share1 --device /dev/dri:/dev/dri --publish 8096:8096 --publish 8920:8920 --env UID=1000 --env GID=100 --env GIDLIST=100 emby/embyserver:latest
 
